d0941f0d4f81a0222f806f6fd59554abc0a1d2cd
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-08 16:56:08 -0700

    messages routes in

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError")
+const Message = require("../models/message");
+const { ensureCorrectUser, ensureLoggedIn } = require("../middleware/auth");
+
+const router = new express.Router();
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -11,6 +22,20 @@
  *
  **/
 
+router.get("/:id", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const msg = await Message.get(req.params.id);
+        if (req.user.username !== msg.from_user.username &&
+            req.user.username !== msg.to_user.username) {
+                return next(new ExpressError("Unauthorized", 401))
+        }
+        else {
+            return res.json({message: msg})
+        }
+    } catch (err) {
+        return next(err)
+    }
+})
 
 /** POST / - post message.
  *
@@ -19,6 +44,19 @@
  *
  **/
 
+router.post("/", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const {to_username, body} = req.body;
+        if (!to_username || !body) {
+            return next(new ExpressError("Message needs to_username and body", 400))
+        }
+        const from_username = req.user.username;
+        const message = await Message.create({from_username, to_username, body});
+        return res.json({message})
+    } catch (err) {
+        return next(err)
+    }
+})
 
 /** POST/:id/read - mark message as read:
  *
@@ -28,3 +66,19 @@
  *
  **/
 
+router.post("/:id/read", ensureLoggedIn, async function(req, res, next) {
+    try {
+        let message = await Message.get(req.params.id);
+        if (req.user.username !== message.to_user.username) {
+            return next(new ExpressError("Only recipient can mark messages read!", 401))
+        }
+        else {
+            message = await Message.markRead(req.params.id)
+            return res.json({message})
+        }
+    } catch (err) {
+        return next(err)
+    }
+})
+
+module.exports = router;
